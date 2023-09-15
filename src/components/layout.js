5668f99a96e1692bdfa5f6292bf16bf78550b3c4
Gandharv More <gandharv.more@pens.com>
2023-09-15 11:30:28 +0530

    pull data from graphql

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 
-const Layout = ({ title, children }) => (
-  <div className="min-h-screen">
-    <nav className="flex gap-4 p-4 underline text-blue-600">
-      <Link to="/">home</Link>
-      <Link to="/about">about</Link>
-    </nav>
-    <main className="flex flex-col items-center justify-center">
-      <h1 className="font-bold text-4xl">{title}</h1>
-      {children}
-    </main>
-  </div>
-);
+const Layout = ({ title, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <div className="min-h-screen">
+      <nav className="flex gap-4 p-4 underline text-blue-600">
+        <h1 className="font-bold !no-underline">
+          {data.site.siteMetadata.title}
+        </h1>
+        <Link to="/">home</Link>
+        <Link to="/about">about</Link>
+      </nav>
+      <main className="flex flex-col items-center justify-center">
+        <h1 className="font-bold text-4xl">{title}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
 
 export { Layout };
